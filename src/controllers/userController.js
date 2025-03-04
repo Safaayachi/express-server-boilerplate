38eece191e4bd7f92a44bda84523545b80a68db0
Safaayachi <safa.ayachi@polytechnicien.tn>
2025-03-04 12:57:33 +0100

    add getUserById and update user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -43,3 +43,55 @@ exports.createUser = async (req, res, next) => {
 		}
 	}
 };
+exports.getUserById = async (req, res, next) => {
+	try {
+		const user = await User.findById(req.params.id);
+
+		if (!user) {
+			return res.status(404).json({
+				success: false,
+				error: "User not found",
+			});
+		}
+
+		res.status(200).json({
+			success: true,
+			data: user,
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			error: "Server Error",
+		});
+	}
+};
+
+exports.updateUser = async (req, res, next) => {
+	try {
+		const user = await User.findByIdAndUpdate(
+			req.params.id,
+			req.body,
+			{
+				new: true,
+				runValidators: true,
+			}
+		);
+
+		if (!user) {
+			return res.status(404).json({
+				success: false,
+				error: "User not found",
+			});
+		}
+
+		res.status(200).json({
+			success: true,
+			data: user,
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			error: "Server Error",
+		});
+	}
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getUsers, createUser } = require("../controllers/userController");
+const {
+	getUsers,
+	createUser,
+	getUserById,
+	updateUser,
+} = require("../controllers/userController");
 
 router.route("/").get(getUsers).post(createUser);
+router.route("/:id").get(getUserById).put(updateUser);
 module.exports = router;
